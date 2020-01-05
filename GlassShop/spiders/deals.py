27cd10099f5236740735eae985a7e3ddac0121fb
hirenvadher954 <hirenvadher954@gmail.com>
2020-01-05 16:19:50 +0530

    Files added

--- GlassShop/spiders/deals.py
+++ GlassShop/spiders/deals.py
@@ -7,11 +7,25 @@ class DealsSpider(scrapy.Spider):
     allowed_domains = ['www.glassesshop.com']
     start_urls = ['https://www.glassesshop.com/bestsellers']
 
+    def get_price(self, selector):
+        original_price = selector.xpath(
+            ".//del/text()").get()
+        if original_price is not None:
+            return original_price
+        else:
+            return selector.xpath(".//div[@class='row']/div[contains(@class, 'pprice')]/span/text()").get()
+
     def parse(self, response):
-        for glass in response.xpath('//div[@class="prlist row"]/div[@class="col-sm-6 col-md-4 m-p-product"]'):
+        glasses = response.xpath("//div[contains(@class, 'm-p-product')]")
+        for glass in glasses:
             yield {
                 'url': glass.xpath(".//div[@class='pimg default-image-front']/a/@href").get(),
-                'img_url': glass.xpath(".//div[@class='pimg default-image-front']/a/img[@src][1]").get(),
-                'title': glass.xpath(".//div[@class='row']/p[@class='pname col-sm-12']/a/text()").get()
-
+                'img_url': glass.xpath(".//div[@class='pimg default-image-front']/a/img[1]/@src").get(),
+                'name': glass.xpath(".//div[@class='row']/p[contains(@class, 'pname')]/a/text()").get(),
+                'price': self.get_price(glass)
             }
+
+        next_page = response.xpath(
+            "//ul[@class='pagination']/li[position() = last()]/a/@href").get()
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse)
